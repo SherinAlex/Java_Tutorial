66677bc0092ab78540716ecd67f39ec0594a2c8e
SherinAlex <alexsherin514@gmail.com>
2024-04-25 13:50:26 +0200

    feat: first code

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable 
+{
+    public static void main(String a[]) 
+    {
+        int num1 = 3;
+        int num2 = 5;
+        int Result = num1 + num2;
+        System.out.println(Result);
+
+
+    }
+}
