c8a8987d42eaeb702cddbd0477d95c42ac5d9947
SherinAlex <alexsherin514@gmail.com>
2024-04-26 22:14:30 +0200

    RO

---
+++ RelationalOperators.java
@@ -0,0 +1,25 @@
+public class RelationalOperators 
+{
+    public static void main(String[] args) 
+    {
+    int x=10;
+    int y=35;
+  /*   boolean result = x < y;   
+    boolean result = x > y;
+    boolean result = x >= y;
+    boolean result = x <= y;
+    boolean result = x != y;
+    */
+    boolean result = x == y;
+    System.out.println(result);
+
+    double a = 7.8;
+    double b = 3.9;
+
+  //  boolean res = a >= b;
+    boolean res = a <= b;
+    System.out.println(res);
+
+    }
+    
+}
