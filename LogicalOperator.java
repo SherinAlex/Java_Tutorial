26527caf87c5b952ea8b9b58de43f37f50b62090
SherinAlex <alexsherin514@gmail.com>
2024-04-26 22:19:36 +0200

    LO

---
+++ LogicalOperator.java
@@ -0,0 +1,24 @@
+public class LogicalOperator 
+{
+public static void main(String[] args) {
+    int x = 7;
+    int y = 9;
+    int a = 48;
+    int b = 44;
+
+    //		boolean result= x>y && a<b ;
+//		boolean result= x>y || a<b ;
+//		boolean result= x>y && a>b ;
+//		boolean result= x>y || a>b ;
+//		boolean result= x<y && a<b ;
+//		boolean result= x<y || a<b ;
+//		boolean result= x<y || a<b || a>1 ;
+		
+//		System.out.println(result);
+		
+		boolean result= a>b ;
+		System.out.println(!result);
+		
+
+}    
+}
