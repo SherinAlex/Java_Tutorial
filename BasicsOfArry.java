eba76ef8906430901ffc7f1a3688710d014f6d5f
SherinAlex <alexsherin514@gmail.com>
2024-05-16 14:53:46 +0200

    basicarr

---
+++ BasicsOfArry.java
@@ -0,0 +1,13 @@
+public class BasicsOfArry 
+{
+    public static void main(String[] args) 
+    {
+        //int num[] = new int[5];  //Array creation
+        int num[] = {2,4,6,8};
+
+        for(int i =0;i<4;i++)
+        {
+            System.out.println(num[i]);
+        }
+    }
+}
