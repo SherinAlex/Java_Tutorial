5ad747007a4296f2abf5c4e54c16f68126bf48ac
SherinAlex <alexsherin514@gmail.com>
2024-05-18 14:32:52 +0200

    immutableandmutable

---
+++ mutimmut.java
@@ -0,0 +1,16 @@
+public class mutimmut 
+{
+    public static void main(String[] args) 
+    {
+        String name = "Sherin";
+    name = name + " Alex";
+    System.out.println(name);
+
+    String s1="Sherin";
+		String s2="Sherin";
+		
+		System.out.println(s1==s2);
+    }
+    	
+		
+}
