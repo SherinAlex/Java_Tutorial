b20675955cef205a679720040f8bec3aceec77d1
SherinAlex <alexsherin514@gmail.com>
2024-05-18 13:35:38 +0200

    Strings

---
+++ str.java
@@ -0,0 +1,15 @@
+public class str 
+{
+    public static void main(String[] args) 
+    {
+       //String name = new String("Sherin");
+       String name = "Sherin";
+
+       System.out.println(name);
+       System.out.println(name.hashCode());
+       System.out.println(name.charAt(4));
+       System.out.println("Best Of Luck " + name);
+       System.out.println(name.concat(" Alex"));
+     
+    }
+}
