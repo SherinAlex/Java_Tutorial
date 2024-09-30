12ee2c8a2a71a963151f03cbc4b7762414622c94
SherinAlex <alexsherin514@gmail.com>
2024-09-30 13:44:30 +0200

    change

--- DownwardAlphaPattern.java
+++ DownwardAlphaPattern.java
@@ -3,11 +3,13 @@ public class DownwardAlphaPattern
     public static void main(String[] args) 
     {
         int a=64;
+        int b=70;
         for(int i=4;i>=1;i--)
         {
             for(int j=1;j<=i;j++)
             {
                 System.out.print((char)(a+j)+" ");
+                System.out.print((char)(b+j)+" ");
             }
             System.out.println();
         }
